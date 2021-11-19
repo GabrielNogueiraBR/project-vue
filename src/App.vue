4659bf523388bdabaecbcf4412d210eeb3bd9f74
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-19 20:01:40 -0300

    feat: add bottom navigation to project

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,104 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-        v-model="drawer"
-        app
-      >
-        <v-list v-model="user">
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img :src="user.picture"></v-img>
-            </v-list-item-avatar>
-          </v-list-item>
-
-          <v-list-item link>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6">
-                {{user.name}}
-              </v-list-item-title>
-              <v-list-item-subtitle>{{user.email}}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item 
-            v-for= "item in items"
-            :key="item.title"
-            :to="item.path"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-{{item.icon}}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{item.title}}</v-list-item-title>
-          </v-list-item>
-          
-        </v-list>
-      </v-navigation-drawer>
-
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Vue Project</v-toolbar-title>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list v-model="user">
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img :src="user.picture"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              {{ user.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.path"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view></router-view>
     </v-main>
 
     <v-footer padless>
-      <v-col
-        class="text-center"
-        cols="12"
-      >
+      <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} — <strong>Gabriel Nogueira</strong>
       </v-col>
     </v-footer>
+
+    <v-bottom-navigation
+      v-model="value"
+      :input-value="active"
+      color="indigo"
+    >
+      <v-btn>
+        <span>Recents</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ 
-      drawer: null,
-      user: {
-        name: 'Satoro Gojo',
-        email: 'satoro_gojo@jujutsu.kaisen.com',
-        picture: 'profile.jpg'
+export default {
+  data: () => ({
+    drawer: null,
+    user: {
+      name: "Satoro Gojo",
+      email: "satoro_gojo@jujutsu.kaisen.com",
+      picture: "profile.jpg",
+    },
+    items: [
+      {
+        title: "Home",
+        icon: "home",
+        path: "/",
       },
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          path: '/'
-        },
-        {
-          title: 'About',
-          icon: 'information-variant',
-          path: '/about'
-        }
-      ]
-    }),
-  }
+      {
+        title: "About",
+        icon: "information-variant",
+        path: "/about",
+      },
+    ],
+  }),
+};
 </script>
