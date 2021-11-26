b6ba03c921e03e022f71d745460208611b32d19b
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-25 21:28:55 -0300

    feat: add cards and informations to home page

---
+++ src/components/EpisodeCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="information.img"
+    >
+      <v-card-title>{{information.title}}</v-card-title>
+    </v-img>
+
+    <v-card-actions>
+      <v-btn
+        text
+      >
+        WATCH
+      </v-btn>
+
+      <v-btn
+        text
+      >
+        DOWNLOAD
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: ['information']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Ola mundo</h1>
+  <v-container>
+    <v-row class="text-center">
+      <v-col
+        v-for="episode in episodes"
+        :key="episode.id"
+        cols="4"
+      >
+        <episode-card :information="episode" ></episode-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+
+import EpisodeCard from "@/components/EpisodeCard"
+
+export default {
+  components:{
+    EpisodeCard
+  },
+  computed:{
+    episodes(){
+      return this.$store.getters.allEpisodes
+    }
+  }
+}
+</script>
